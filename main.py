8423d86638a3e5a11c0c607df70003a03b6a3054
LeilanysA <larzate@hawkmail.hccfl.edu>
2025-04-10 16:13:36 -0400

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+# Inputs 
+employeeName = input("Enter the employee's name: ")
+numShifts = int(input("Enter the number of shifts worked: "))
+numTranscations = int(input("Enter the number of transcations: "))
+transcationValues = float(input("Enter the total dollar value of transcations: "))
 
+# IF and ELSE statements
+if numShifts > 0 and numTranscations > 0:
+    productivityScore = (transcationValues / numTranscations) / numShifts
+
+    if productivityScore <= 30:
+        bonus = 50.00
+    else:
+      if productivityScore <= 69:
+        bonus = 75.00
+      else:
+        if productivityScore <= 199:
+          bonus = 100.00
+        else:
+          bonus = 200.00
+# Final output
+print(f"Employee Name: {employeeName}")
+print(f"Employee Bonus: ${bonus}")
